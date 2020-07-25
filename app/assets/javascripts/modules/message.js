530a54a7d63c499f9cdcc2876eaba5e5fff19812
noritanyutan <67891057+noritanyutan@users.noreply.github.com>
2020-07-25 13:13:53 +0900

    S3

--- app/assets/javascripts/modules/message.js
+++ app/assets/javascripts/modules/message.js
@@ -54,9 +54,9 @@ $(function(){
     })
     .done(function(data){
       let html = buildHTML(data);
-      $('.MessageField').append(html);      
+      $('.message-list').append(html);      
       $('form')[0].reset();
-      $('.MessageField').animate({ scrollTop: $('.MessageField')[0].scrollHeight});
+      $('.message-list').animate({ scrollTop: $('.message-list')[0].scrollHeight});
       $('.Form__submit').prop("disabled", false);
     })
     .fail(function() {
